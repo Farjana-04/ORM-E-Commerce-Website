2eb65cac7e05ede7257b643892c061b567210c7b
Farjana Akhter <farjana.akhter085@gmail.com>
2023-09-14 11:32:51 -0500

    create and update category in routes/api/category-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,39 +44,37 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-
 router.post('/', async (req, res) => {
-//   // create a new category
-//   try {
-//     const categoryData = await Category.create({
-//       category_name: req.body.category_name
-//     })
-//     res.status(200).json(categoryData);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+  // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// router.put('/:id', async (req, res) => {
-//   // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
 
-//   try {
-//     const categoryData = await Category.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-//       individualHooks: true
-//     });
-//     if (!categoryData[0]) {
-//       res.status(404).json({ message: 'No category with this id!' });
-//       return;
-//     }
-//     res.status(200).json(categoryData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+      individualHooks: true
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // router.delete('/:id', async (req, res) => {
 //   // delete a category by its `id` value
 //   try {
@@ -94,7 +92,7 @@ router.post('/', async (req, res) => {
 //   } catch (err) {
 //     res.status(500).json(err);
 //   }
-});
+// });
 
 
 
