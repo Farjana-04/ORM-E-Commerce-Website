f3eee0461ac943eb76530a62de6dd9323cad7bff
Farjana Akhter <farjana.akhter085@gmail.com>
2023-09-14 12:02:38 -0500

    added find all tags and a single tag by its id in routes/api/tag-routes.js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,54 +3,54 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-// router.get('/', async (req, res) => {
-//   // find all tags
-//   // be sure to include its associated Product data
-//   try {
-//     const tagData = await Tag.findAll({
-//       attributes: ['id', 'tag_name'],
-//       include: [
-//         {
-//             model: Product,
-//             attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-//         },
-//       //   {
-//       //     model: ProductTag,
-//       //     attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-//       // }
-//     ]
-//     });
-//     res.status(200).json(tagData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      attributes: ['id', 'tag_name'],
+      include: [
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        },
+      //   {
+      //     model: ProductTag,
+      //     attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      // }
+    ]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/:id', async(req, res) => {
-//   // find a single tag by its `id`
-//   // be sure to include its associated Product data
-//   //we can use findOne()
-//  try {
-//       const tagData = await Tag.findByPk(req.params.id, {
-//         // attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-//         include: [
-//           {
-//               model: Product,
-//               attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-//           }
-//       ]
-//       });
+router.get('/:id', async(req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  //we can use findOne()
+ try {
+      const tagData = await Tag.findByPk(req.params.id, {
+        // attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        include: [
+          {
+              model: Product,
+              attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+          }
+      ]
+      });
 
-//       if (!tagData) {
-//         res.status(404).json({ message: 'No tag found with that id!' });
-//         return;
-//       }
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag found with that id!' });
+        return;
+      }
 
-//       res.status(200).json(tagData);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 // router.post('/', async(req, res) => {
 //   // create a new tag
 //   try {
